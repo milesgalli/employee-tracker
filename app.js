6a2d6419b10189ac8190083ab7b7e66a54538da9
mgalli <galli.miles@gmail.com>
2020-05-12 16:48:06 +0800

    ädding readme, fixing department.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const addDepartment = require("./department");
+// const addDepartment = require("./department");
+
 
 const con = mysql.createConnection({
   host: "localhost", //Where my SQL is localhost = your machine
@@ -12,7 +13,7 @@ const con = mysql.createConnection({
 con.connect(function (err) {
   //Print out the error and exit id you encounter an error
   if (err) {
-    console.err("coooked it");
+    console.err("Error not connected ");
     return;
   }
   //succcessful connection
@@ -20,29 +21,7 @@ con.connect(function (err) {
   console.log("Sucess!");
 });
 
-// con.query(
-//   "SELECT `first_name`, `last_name`, `title` as 'role' FROM employeesdb.employee inner join employeesdb.roles on(employee.role_id = roles.id);",
-//   function (err, resul
-// ts, fields) {
-//     console.table(results);
-//   }
-// );
-
-// PSEUDO CODE
-
-// Add departments, roles, employees
-
-// View departments, roles, employees
-
-// Update employee roles
-
-// inquirer for add, view and update delete.
-
-// create a function so if you select one of the chocies it pops up for the selected chocie.
-
-// in the promsie use the the resposne tp
-
-function start() {
+function start(con) {
   inquirer
     .prompt([
       {
@@ -55,7 +34,7 @@ function start() {
           "View all roles",
           "Add Employee, role, department",
           "Update employee role",
-         
+
           //"Update manager role"
         ],
       },
@@ -64,23 +43,23 @@ function start() {
       console.log(action);
       if (action === "View all employees") {
         viewEmployees();
-
       } else if (action === "View all employees by department") {
         viewEmployeesDepartment();
-
       } else if (action === "View all roles") {
         viewRoles();
-
       } else if (action === "Add Employee, role, department") {
         add();
+        return 
       } else {
-        UpdateEmployees();
+        updateEmployeeRoles();
       }
+      async.await()
+      start(); 
     });
+    
 }
 
 function add() {
-  let questions = "";
   inquirer
     .prompt([
       {
@@ -93,7 +72,7 @@ function add() {
     .then(({ question }) => {
       console.log(question);
       if (question === "department") {
-        addDepartment();
+        addDepartment(con);
       } else if (question === "role") {
         addRole();
       } else if (question === "employee") {
@@ -101,6 +80,33 @@ function add() {
       }
     });
 }
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "what is the name of the department you want to add?  ",
+      },
+    ])
+    .then(function (answer) {
+      con.query(
+        "INSERT INTO department SET ?",
+ 
+        {
+          title: answer.name
+        },
+        // connection.query("UPDATE posts SET title = :title", { title: "Hello MySQL" });
+        function (err) {
+          if (err) throw err;
+          console.log("Your auction was created successfully!");
+          // re-prompt the user for if they want to bid or post
+         start()
+        }
+      );
+    });
+ }
 
 function addRole() {
   inquirer
@@ -182,26 +188,7 @@ function addEmployee() {
     });
 }
 
-function viewEmployees (){
-
-con.query(
-  "SELECT `first_name`, `last_name`, `title` as 'role' FROM employeesdb.employee inner join employeesdb.roles on(employee.role_id = roles.id);",
-  function (err, results, fields) {
-    console.table(results);
-  }
-);
-}
-
-function viewEmployeesDepartment(){
-  con.query(
-    "SELECT  `first_name`, last_name,  as 'department' FROM employeesdb.department inner join employeesdb.employee on (employee.department = department);",
-    function (err, results, fields) {
-      console.table(results);
-    }
-  );
-}
-
-function viewRoles(){
+function viewEmployees() {
   con.query(
     "SELECT `first_name`, `last_name`, `title` as 'role' FROM employeesdb.employee inner join employeesdb.roles on(employee.role_id = roles.id);",
     function (err, results, fields) {
@@ -210,5 +197,54 @@ function viewRoles(){
   );
 }
 
+function viewEmployeesDepartment() {
+  con.query(
+    "SELECT  `first_name`, last_name,  as 'department' FROM employeesdb.department inner join employeesdb.employee on (employee.department = department);",
+    function (err, results, fields) {
+      console.table(results);
+    }
+  );
+}
+
+function viewRoles() {
+  con.query(
+    "SELECT `first_name`, `last_name`, `title` as 'role' FROM employeesdb.employee inner join employeesdb.roles on(employee.role_id = roles.id);",
+    function (err, results, fields) {
+      console.table(results);
+    }
+  );
+}
+
+function updateEmployeeRoles() {
+  inquirer
+    .prompt([
+      {
+        name: "id",
+        message: "what is the employee id you would like to udate?",
+      },
+      {
+        name: "title",
+        message: "what is the new title ?",
+      },
+    ])
+
+    .then(function (answer) {
+      con.query(
+        "UPDATE roles SET title = ? WHERE id = ?",
+
+        // onnection.query('UPDATE users SET foo = ?, bar = ?, baz = ? WHERE id = ?', ['a', 'b', 'c', userId], function (error, results, fields) {
+        //   if (error) throw error;
+        //   // ...
+        // });
+        [ answer.title, answer.id],
+
+        function (err) {
+          if (err) throw err;
+          console.log("Your action was created successfully!");
+          // re-prompt the user for if they want to bid or post
+        }
+      );
+    });
+}
 
 start();
---
+++ readme.md
@@ -0,0 +1,36 @@
+# Overview 
+
+This application is a content manager application which allows the user to interface with the company database. 
+
+# Install 
+
+To use this application install the folowing 
+* inquirer 
+* mysql 
+
+Before running the applicaiton make sure the database is connected. If you see the success message then the database is not connected. 
+
+To start the app use the command node .\app.js. 
+
+# Functionality 
+
+View 
+* View all employees 
+* View all employees by department 
+* View all roles
+
+Add 
+* Add Department
+* Add Employee
+* Add Role
+
+Update 
+* Employees role  
+
+# Final Thoughts 
+
+This application could be refractored and organised better.I could have used different files to house all the main functions. However this required having a high level understanding of async await and promises. I am imporving my understanding of promises, however this area needs some work. 
+
+I need to improve my knowledge of table joins, as i found this to be particulary confusing. I did enjoy using a database as it demonstrated an important part of softare engineering. 
+
+ Although this is the basic version of the assingment, if i had added on the bonus it would have been alot longer. Im becoming more confident in writing my own code, howver it is often small things that trip me up. I have a strong understanding of what to build and how to build it, but my execution needs work. 
